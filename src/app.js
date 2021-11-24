26572e939d2bcf3fdcc09a8b431eb3a3a1565549
oislasreyes <oislasreyes@gmail.com>
2021-11-23 20:21:31 -0600

    Agregando la documentacion al proyecto

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/extensions */
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import userRoutes from './routes/users.routes.js';
 import statusRoutes from './routes/status.routes.js';
 import handleErrors from './middlewares/error.middleware.js';
+import swaggerDocument from './swagger.json';
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(userRoutes);
